d5c8248ed263cf3d78ce5e2769fa348891285572
GodsplanELot <elotgodsplan2@gmail.com>
2025-01-03 15:21:44 +0100

    finished routing now to middleware

--- server.js
+++ server.js
@@ -26,7 +26,22 @@ app.get('/hello(.html)?', (req, res, next) => {
 });
 
 //chaining route handlers
+const one = (req, res, next) => {
+    console.log('one');
+    next();
+}
 
+const two = (req, res, next) => {
+    console.log('two');
+    next();
+}
+
+const three = (req, res) => {
+    console.log('three');
+    res.send('Finished!');
+}
+
+app.get('/chain(.html)?', [one, two, three]);
 
 
 
